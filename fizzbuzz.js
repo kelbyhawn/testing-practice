8c0d3c098bf5ca0f0efdcb41aa13476801e7bf39
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2022-02-10 15:25:32 -0800

    refactor and add comments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,15 @@
 function fizzBuzz(numbers) {
+  // start with an empty array
   let result = [];
 
   for (number of numbers) {
-    if (number % 15 === 0) {
-      result.push('fizzbuzz');
-    } else if (number % 3 === 0) {
-      result.push('fizz');
-    } else if (number % 5 === 0) {
-      result.push('buzz');
-    } else {
-      result.push(number);
-    }
+    if (number % 15 === 0) result.push('fizzbuzz');
+    else if (number % 3 === 0) result.push('fizz');
+    else if (number % 5 === 0) result.push('buzz');
+    else result.push(number);
   }
 
+  // return the array as a string
   return result.join(', ');
 
 }
